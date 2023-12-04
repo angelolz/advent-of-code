2cd2f56d8f2b49d70e04f9d70204de0ad2ca1755
angelolz <angel.lugia98@gmail.com>
2023-12-04 16:30:38 -0700

    added part 2

--- 2023/1.java
+++ 2023/1.java
@@ -1,7 +1,14 @@
 public class Day1
 {
-    public static void main(String[] args) throws IOException {
+	//this was for part 2
+	static String[][] wordDigits = new String[][]{{"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"}, {"five", "5"}, {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}};
 
+    public static void main(String[] args) throws IOException {
+		partOne();
+		partTwo();
+	}
+
+	public static void partOne() {
 		List<String> lines = Files.readAllLines(Paths.get("input.txt"));
 
 		int answer = 0;
@@ -27,7 +34,52 @@ public class Day1
 		System.out.println(answer);
 	}
 
-	public static boolean isDigit(char c) {
+	
+
+	public static void partTwo() {
+		List<String> lines = Files.readAllLines(Paths.get("input.txt"));
+
+		int answer = 0;
+		for (String line : lines) {
+			System.out.println(line);
+			StringBuilder sb = new StringBuilder();
+			int start = 0;
+			for (int i = 0; i < line.length(); i++) {
+				String sub = line.substring(start, i + 1);
+
+				int wordIndex = Arrays.stream(wordDigits).map(word -> sub.indexOf(word[0])).filter(idx -> idx >= 0).sorted().findFirst().orElse(-1);
+				if (wordIndex >= 0) {
+					for (String[] wordDigit : wordDigits) {
+						if (wordDigit[0].equals(sub.substring(wordIndex))) {
+							sb.append(wordDigit[1]);
+							start = i;
+							break;
+						}
+					}
+				}
+
+				if (isDigit(line.charAt(i))) {
+					sb.append(line.charAt(i));
+					start = i + 1;
+				}
+			}
+
+			if(sb.length() == 1) {
+				System.out.println("   - " + Integer.parseInt("" + sb.charAt(0) + sb.charAt(0)));
+				answer += Integer.parseInt("" + sb.charAt(0) + sb.charAt(0));
+			}
+
+			else {
+				System.out.println("   - " + Integer.parseInt("" + sb.charAt(0) + sb.charAt(sb.length() - 1)));
+				answer += Integer.parseInt("" + sb.charAt(0) + sb.charAt(sb.length() - 1));
+
+			}
+		}
+
+		System.out.println("answer: " + answer);
+	}
+	
+	private static boolean isDigit(char c) {
 		return (c >= '0' && c <= '9');
 	}
 }
