f57b2028a1fa1adaa8786acf7e35b85218735815
angelolz <angel.lugia98@gmail.com>
2023-12-05 20:09:28 -0700

    better attempt at day 3 part 1

--- 2023/Day3.java
+++ 2023/Day3.java
@@ -12,15 +12,18 @@ public class Day3
         //        partTwo();
     }
 
-    private static class PartNum {
+    private static class PartNum
+    {
         String num;
         int x;
         int y;
 
-        public PartNum() {
+        public PartNum()
+        {
             this.x = -1;
             this.y = -1;
         }
+
         public String getNum()
         {
             return num;
@@ -46,43 +49,46 @@ public class Day3
         {
             return y;
         }
-
-        public boolean isLocationSet() {
-            return x < 0 || y < 0;
-        }
     }
 
     private static void partOne() throws IOException
     {
-
         List<String> lines = Files.readAllLines(Paths.get("input.txt"));
-        int maxRows = lines.size();
-        int maxCol = lines.get(0).length(); //this is because all lines are the same length
         int answer = 0;
-        List<Integer[]> symbols = new ArrayList<>();
+        List<PartNum> numbers = extractPartNumbers(lines);
+
+
+        for(PartNum part : numbers)
+        {
+            if(checkCoord(lines, part))
+            {
+                System.out.println(part.getNum() + " is a part num");
+                answer += Integer.parseInt(part.getNum());
+            }
+        }
+
+        System.out.println("Answer: " + answer);
+    }
+
+    private static List<PartNum> extractPartNumbers(List<String> lines)
+    {
         List<PartNum> numbers = new ArrayList<>();
+        int maxRows = lines.size();
+        int maxCols = lines.get(0).length();
 
         for(int i = 0; i < maxRows; i++)
         {
             StringBuilder str = new StringBuilder();
             PartNum partNum = null;
 
-            for(int j = 0; j < maxCol; j++) {
+            for(int j = 0; j < maxCols; j++)
+            {
                 char c = lines.get(i).charAt(j);
 
-                if(isSymbol(c)) {
-                    symbols.add(new Integer[]{j, i});
-
-                    if(partNum != null) {
-                        partNum.setNum(str.toString());
-                        numbers.add(partNum);
-                        str = new StringBuilder();
-                        partNum = null;
-                    }
-                }
-
-                else if(Character.isDigit(c)) {
-                    if(partNum == null) {
+                if(Character.isDigit(c))
+                {
+                    if(partNum == null)
+                    {
                         partNum = new PartNum();
                         partNum.setLocation(j, i);
                     }
@@ -90,7 +96,8 @@ public class Day3
                     str.append(c);
                 }
 
-                else if(partNum != null) {
+                else if(partNum != null)
+                {
                     partNum.setNum(str.toString());
                     numbers.add(partNum);
                     str = new StringBuilder();
@@ -98,70 +105,53 @@ public class Day3
                 }
             }
 
-            if(partNum != null) {
+            if(partNum != null)
+            {
                 partNum.setNum(str.toString());
                 numbers.add(partNum);
             }
         }
 
-        for(PartNum num : numbers) {
-            boolean isValid = checkCoord(num, symbols, maxRows, maxCol);
-            if(isValid) {
-                System.out.println(num.getNum() + " is a part num");
-                answer += Integer.parseInt(num.getNum());
-            }
-        }
-
-        System.out.println("Answer: " + answer);
+        return numbers;
     }
 
-    private static boolean checkCoord(PartNum num, List<Integer[]> symbols, int maxRows, int maxColumns) {
-        boolean valid;
-        for(int i = 0; i < num.getNum().length(); i++) {
-            if(i == 0) //beginning
-                valid = checkLeft(num, symbols, maxRows, maxColumns);
-            else if( i == num.getNum().length() - 1) //end
-                valid = checkRight(num, symbols, i, maxRows, maxColumns);
-            else //somewhere in between
-                valid = checkMiddle(num, symbols, i, maxRows, maxColumns);
+    private static boolean checkCoord(List<String> lines, PartNum part)
+    {
+        return checkTop(lines, part) || checkSides(lines, part) || checkBottom(lines, part);
+    }
 
-            if(valid) return valid;
-        }
+    private static boolean checkTop(List<String> lines, PartNum part)
+    {
+        if(part.getY() - 1 < 0) return false;
+
+        String sub = lines.get(part.getY() - 1).substring(Math.max(0, part.x - 1), Math.min(lines.get(part.getY()).length(), part.x + part.getNum().length() + 1));
+        return hasSymbol(sub);
+    }
+
+    private static boolean checkSides(List<String> lines, PartNum part)
+    {
+        return (part.getX() - 1 > 0 && isSymbol(lines.get(part.getY()).charAt(part.getX() - 1)))
+            || (part.getX() + part.getNum().length() < lines.get(part.getY()).length() && isSymbol(lines.get(part.getY()).charAt(part.getX() + part.getNum().length())));
+    }
+
+    private static boolean checkBottom(List<String> lines, PartNum part)
+    {
+        if(part.getY() + 1 >= lines.size()) return false;
+
+        String sub = lines.get(part.getY() + 1).substring(Math.max(0, part.x - 1), Math.min(lines.get(part.getY()).length(), part.x + part.getNum().length() + 1));
+        return hasSymbol(sub);
+    }
+
+    private static boolean isSymbol(char c)
+    {
+        return !Character.isDigit(c) && c != '.';
+    }
+
+    private static boolean hasSymbol(String str)
+    {
+        for(char c : str.toCharArray())
+            if(isSymbol(c)) return true;
 
         return false;
-    }
-
-    private static boolean checkLeft(PartNum num, List<Integer[]> symbols, int maxRows, int maxColumns) {
-        if(checkSymbol(num.x - 1, num.y - 1, symbols, maxRows, maxColumns)) return true;
-        if(checkSymbol(num.x - 1, num.y, symbols, maxRows, maxColumns)) return true;
-        if(checkSymbol(num.x - 1, num.y + 1, symbols, maxRows, maxColumns)) return true;
-
-        return checkMiddle(num, symbols, 0, maxRows, maxColumns);
-    }
-
-    private static boolean checkRight(PartNum num, List<Integer[]> symbols, int i, int maxRows, int maxColumns) {
-        if(checkSymbol(num.x + 1 + i, num.y + 1, symbols, maxRows, maxColumns)) return true;
-        if(checkSymbol(num.x + 1 + i, num.y, symbols, maxRows, maxColumns)) return true;
-        if(checkSymbol(num.x + 1 + i, num.y - 1, symbols, maxRows, maxColumns)) return true;
-
-        return checkMiddle(num, symbols, i, maxRows, maxColumns);
-    }
-
-    private static boolean checkMiddle(PartNum num, List<Integer[]> symbols, int i, int maxRows, int maxColumns) {
-        if(checkSymbol(num.x + i, num.y + 1, symbols, maxRows, maxColumns)) return true;
-        return checkSymbol(num.x + i, num.y - 1, symbols, maxRows, maxColumns);
-    }
-
-    private static boolean checkSymbol(int x, int y, List<Integer[]> symbols, int maxRows, int maxColumns) {
-        if(!inRange(x, y, maxRows, maxColumns)) return false;
-        return symbols.stream().anyMatch(coords -> coords[0] == x && coords[1] == y);
-    }
-
-    private static boolean inRange(int x, int y, int maxRows, int maxColumns) {
-        return x >= 0 && y >= 0 && x < maxColumns && y < maxRows;
-    }
-
-    private static boolean isSymbol(char c) {
-        return !Character.isDigit(c) && c != '.';
     }
 }
--- 2023/README.md
+++ 2023/README.md
@@ -1,12 +1,17 @@
 # 2023 Advent of Code
-## Day 1
+## Day 1 (completed on 12/4/23)
 In my head, solving this was pretty easy. Although the second part was pretty tough. I watched my [partner](https://github.com/AzureToast) work on this and learned a few things:
 
 - Instead of doing `for(int i = 0; i < line.length(); i++)`, I could've just done `for(char c : line.toCharArray())`.
 - When I was getting a new substring in part two, I feel like I made it more complicated than I should have. I think that I could've just kept adding chars to the substring, and if a digit or word number was found, just empty the substring.
 - I skimmed through some solutions on the subreddit, and noticed some people using regex for part 2. That was one of my possible solutions, but I just couldn't make it work for some reason lol
 
-## Day 2
+## Day 2 (completed on 12/4/23)
 This was pretty easy, although I did use ChatGPT to help me figure out a good regex for getting the number of cubes and their color in each game.
 
 I do feel like I could've found a better way to get the ID of the game for the first part, though.
+
+## Day 3 (completed on 12/5/23)
+This day had me in a chokehold. All the high I got from solving Day 2 quickly went away fast. I was working on Part 1 the same day I finished Day 1 and 2, and I was writing the hottest garbage code that I have written in a while. Decided to skip to Day 4 and work on that one for a bit before taking a break.
+
+On the next day, I figured out a different (and better way) to determine symbols. Instead of checking each symbol around the number, I can just get the substring from the top and bottom, and then check the sides, then determining if any of them had symbols. That worked!
